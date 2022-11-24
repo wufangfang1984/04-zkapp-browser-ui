234abfbffc0e0c33a290b82829084aad524a8288
crackerli <crackerli@163.com>
2022-11-24 14:33:33 +0800

    repo init

--- contracts/src/Add.ts
+++ contracts/src/Add.ts
@@ -12,8 +12,8 @@ import { Field, SmartContract, state, State, method } from 'snarkyjs';
 export class Add extends SmartContract {
   @state(Field) num = State<Field>();
 
-  init() {
-    super.init();
+  @method init() {
+    //    super.init();
     this.num.set(Field(1));
   }
 
@@ -21,6 +21,7 @@ export class Add extends SmartContract {
     const currentState = this.num.get();
     this.num.assertEquals(currentState); // precondition that links this.num.get() to the actual on-chain state
     const newState = currentState.add(2);
+    newState.assertEquals(currentState.add(2));
     this.num.set(newState);
   }
 }
